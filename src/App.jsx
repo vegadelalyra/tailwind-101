8501d6eb3d334a602954214f1bb95754451f580e
vegadelalyra <lyradeperseo@gmail.com>
2023-09-14 05:19:59 -0500

    feat: create discord-stylish sidebar react component with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import SideBar from './SideBar'
+
+function App() {
+    return (
+        <>
+            {/* <div className='text-3xl font-bold underline'>tailwind</div>
+            <p className='text-center text-green-500 font-bold text-5xl'>
+                hi mom!
+            </p> */}
+
+            <div className="flex">
+                <SideBar />
+            </div>
+        </>
+    )
+}
+
+export default App
